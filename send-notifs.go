35589417dd4f58cba0f742c06042cea8fd24b555
Sujay1844 <sujay1844@protonmail.com>
2022-07-09 23:40:50 +0530

    Completed

--- send-notifs.go
+++ send-notifs.go
@@ -5,21 +5,44 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"strconv"
+	"strings"
 
 	"github.com/martinlindhe/notify"
 )
 
 func main() {
-	notify.Notify("Updates", "foo", "", "")
-	app := "echo"
-	arg0 := "bar"
-	
-	cmd := exec.Command(app, arg0)
-	stdout, err := cmd.Output()
+	OS := "fedora"
+	stdout := ""
 
-	if err != nil {
-		fmt.Println(err.Error())
+	if OS == "fedora" {
+		stdout = fedora()
+	} else if OS == "arch" {
+		stdout = arch()
 	}
 
-	fmt.Println(string(stdout))
+	fmt.Println(stdout)
+	line_count := strings.Count(stdout, "updates")
+	if line_count == 0 {
+		fmt.Println("No updates available")
+		return
+	}
+	notify_str := strconv.Itoa(line_count) + " updates are available"
+	notify.Notify("Updates", notify_str, "", "")
+}
+
+func fedora() string {
+	app := "dnf"
+	arg0 := "check-update"
+	arg1 := "-q"
+	cmd := exec.Command(app, arg0, arg1)
+	stdout, _ := cmd.Output()
+	return string(stdout)
+}
+
+func arch() string {
+	app := "check-updates"
+	cmd := exec.Command(app)
+	stdout, _ := cmd.Output()
+	return string(stdout)
 }
